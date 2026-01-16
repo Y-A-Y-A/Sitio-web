350a121123ede7dc62a2751a534459c072168580
yaya <240765@gmail.com>
2026-01-16 09:56:47 -0600

    claculadora

--- js/funcion.js
+++ js/funcion.js
@@ -1,3 +1,25 @@
 function mostrarAlerta(){
     alert("Hola Bienvenido al Sitio Web");
 }
+function suma(){
+let valor1=5;
+let valor2=10;
+let resultado = valor1 + valor2;
+console.info("El resultado de la suma es: "+ resultado);
+}
+function sumaParam(valor1, valor2){
+    console.info("El valor 1:"+ valor1);
+    console.info("El valor 2:"+ valor2);
+}
+
+function sumarInput(){
+    let val1 = document.getElementById("val1").value;
+    let val2 = document.getElementById("val2").value;
+    let res = parseFloat(val1)+ parseFloat(val2);
+    console.info("El resultado de la suma es:" +res);
+    if(res>10){
+        document.getElementById("mensaje").value="Numero mayor a 10";
+    }else{
+        document.getElementById("mensaje").value ="numero menor"
+    }
+}
